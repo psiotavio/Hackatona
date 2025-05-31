309f2dafc752c344a3ffe2e7d9747dd7d742303c
Eduardo Godoi <eduadg@hotmail.com>
2025-05-31 02:53:20 -0300

    feat: Create login page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,7 +15,7 @@ export {
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: 'login',
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -42,6 +42,7 @@ function RootLayoutNav() {
           },
         }}
       >
+        <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen 
           name="modal" 
